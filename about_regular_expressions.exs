69ec5701104a59f99501522301eb0957681fdb13
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-04-15 14:56:34 -0700

    Use `~r/.../` as the default regex sigil
    The elixir documentation is using `~r/.../`

--- about_regular_expressions.exs
+++ about_regular_expressions.exs
@@ -20,23 +20,23 @@ defmodule AboutRegularExpressions do
   think "Regular expressions can be created with sigils" do
     regex = Regex.compile!(__?)
 
-    assert regex == ~r{this works!}
+    assert regex == ~r/this works!/
   end
 
   think "Sigils have flexible delimiters" do
     # Hint: Don't use Regex.compile here, use a sigil instead
-    assert __? == ~r/this works!/
+    assert __? == ~r(this works!)
   end
 
   think "Capture groups" do
-    regex = ~r{(A|The) lazy bear slept for (\d+) months}
+    regex = ~r/(A|The) lazy bear slept for (\d+) months/
     text = "The lazy bear slept for 5 months"
     assert Regex.run(regex, text) == __?
   end
 
   think "Replacing with capture groups" do
     text = "This has vowels"
-    regex = ~r{([aeiou])}
+    regex = ~r/([aeiou])/
     assert Regex.replace(regex, text, "<\\1>") == __?
   end
 end
